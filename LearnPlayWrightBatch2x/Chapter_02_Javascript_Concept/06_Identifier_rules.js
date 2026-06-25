c84a32182cf4fdafa5298dfa622f4951935ce3e4
AlakKhanna <alak.khanna@gmail.com>
2026-06-25 18:31:57 +0530

    Add missing Chapter 02 JavaScript files

---
+++ LearnPlayWrightBatch2x/Chapter_02_Javascript_Concept/06_Identifier_rules.js
@@ -0,0 +1,25 @@
+ var $ = 10;
+ var _a = 20;
+ var v = 30;
+ var V = 40;
+ console.log($);
+ console.log(_);
+ console.log(v);
+ console.log(V);
+ var abc123= 50;
+ console.log(abc123);
+
+ // var 123abc= 60; // This will throw an  error because identifiers cannot start with a number
+
+ var Name = "John";
+    console.log(Name);
+
+var name = "Doe";
+    console.log(name);
+
+// var name 1= "Smith"; // This will throw an error because identifiers cannot contain spaces
+
+var name123 = "Smith";
+    console.log(name123);
+    var name_1 = "Smith";
+    console.log(name_1); 
