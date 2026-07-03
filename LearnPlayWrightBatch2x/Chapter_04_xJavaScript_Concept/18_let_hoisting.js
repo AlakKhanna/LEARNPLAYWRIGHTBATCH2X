38aad06c34802373df82442b632faeb6095295fd
AlakKhanna <alak.khanna@gmail.com>
2026-07-03 18:51:12 +0530

    Add Chapter 04 additional files: hoisting and QA exercises

---
+++ LearnPlayWrightBatch2x/Chapter_04_xJavaScript_Concept/18_let_hoisting.js
@@ -0,0 +1,7 @@
+
+
+console.log(a); // ReferenceError: Cannot access 'a' before initialization
+let a = 10;
+console.log(a); // Output: 10   
+let b = 20;
+console.log(b); // Output: 20
---
+++ LearnPlayWrightBatch2x/Chapter_04_xJavaScript_Concept/21_jr_qa.js
@@ -0,0 +1,8 @@
+console.log(API_END_APP_VWO_COM);
+console.log("data");
+if (true) {
+
+}
+
+var API_END_APP_VWO_COM = "https://app.vwo.com";
+console.log(API_END_APP_VWO_COM);
